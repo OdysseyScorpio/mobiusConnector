8121f875e564cad294be6f93e8ab79d3b5dfb1ff
martyn.robert.green@gmail.com <martyn.robert.green@gmai.com>
2018-09-01 13:32:00 +0100

    loaded working plugin and added route to get version

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,6 @@ var MongoClient = require('mongodb').MongoClient, format = require('util').forma
 var Event = require('../models/events');
 
 router.use(function(req,res,next){
-	console.log('Something is happenning');
 	next();//make sure we go to the next routes and dont stop there
 });
 
@@ -19,106 +18,43 @@ router.get('/',function(req,res){
 router.post('/events',function(req,res){
 	var newEvent = new Event();//create a new instance of the event model
 	newEvent.name = req.body;
-
-MongoClient.connect('mongodb://127.0.0.1/events', function(err,db) {
-console.log("Connecting to DB")
+	MongoClient.connect('mongodb://127.0.0.1/events', function(err,db) {
     if (err) throw err;
-console.log("Connected to Database");
-       var document = req.body;
 
-var dbo=db.db("events")
-    // insert record
+		var document = req.body;
+		var dbo=db.db("events")
+	    // insert record
         dbo.collection('events').insert(document, function(err, records) {
-
-
-        if (err) throw err;
-        console.log("Record added as " + records.insertedIds);
-	res.json({message:'Event added successfully'});	
-	db.close();
-    });
+	        if (err) throw err;
+			res.json({message:'Event added successfully'});	
+			db.close();
+	    });
+	});
 });
-});
-
-
 
 router.get('/news',function(req,res){
 
-        MongoClient.connect('mongodb://127.0.0.1/events', function(err,db) {
-        var dbo=db.db("events")
-        console.log("Start");
-	
-
-//	dbo.collection('news').find({},{_id:0,update:1},{sort:'-timestamp'}).limit(1,function(err,items){
-//	console.log(items)
-//	console.log("here")
-//	console.log(err)
-//	})
-
-
-
-        console.log("Start");
-	//db.news.find({},{_id:0,update:1}).sort({timestamp:-1}).pretty()
-	//dbo.collection('news').find   ({timestamp:{$exists:true}},{_id:0,update:1},{sort:{timestamp:-1}}).limit(2,function(err,items){
-
-	//dbo.collection('news').findOne({timestamp:{$exists:true}}},{update:1,_id:0}).sort({timestamp:-1}),function(err,items) {
-	//dbo.collection('news').findOne({timestamp:{$exists:true}},{update:1,_id:0},function(err,items) {
-	  dbo.collection('news').find({timestamp:{$exists:true}},{update:1,_id:0}).toArray(function(err,items) {
-	
-
-console.log(items)
-        if (err) throw err;
-	update =items[items.length-1]
-        res.json({update});
-     });
-  });
-
-});
-
-
-
-
-router.get('/events',function(req,res){
-	Event.find(function(err,events){
-		if(err)
-			res.send(err);
-		
-		res.json(events);
-	});
-});
-
-router.get('/events/:id',function(req,res){
-	Event.findById(req.params.id,function(err,event){
-		if(err)
-			res.send(err);
-		
-		res.json(event);
-	});
-});
-
-router.put('/events/:id',function(req,res){
-	Event.findById(req.params.id,function(err,event){
-		if(err)
-			res.send(err);
-		
-		event.name = req.body.name;//update the event name
-		
-		//save the event after change
-		event.save(function(err){
-			if(err)
-				res.send(err);
-			
-			res.json(event);
+    MongoClient.connect('mongodb://127.0.0.1/events', function(err,db) {
+    	var dbo=db.db("events")
+    	dbo.collection('news').find({timestamp:{$exists:true}},{update:1,_id:0}).toArray(function(err,items) {
+			if (err) throw err;
+			update =items[items.length-1]
+    		res.json({update});
 		});
 	});
 });
 
-router.delete('/events/:id',function(req,res){
-	Event.remove({'_id':req.params.id},function(err){
-		if(err)
-			res.end(err);
-		
-		res.json({ message:'Event Successfully Deleted' });
+router.get('/version',function(req,res){
+
+    MongoClient.connect('mongodb://127.0.0.1/events', function(err,db) {
+    	var dbo=db.db("version")
+    	dbo.collection('version').findOne({timestamp:{$exists:true}},{version:1,_id:0},function(err,items) {
+			if (err) throw err;
+			version =items[0].version
+    		res.json({version});
+		});
 	});
 });
+
 
 module.exports = router;
