046fc3a796c6a9874d73b3449e4e29a4930dfce4
OdysseyScorpio <martyn.robert.green@gmail.com>
2018-09-10 00:09:12 +0200

    Modified api route and changed webpage to show completed missions with data from missionAccepted and missionCompleted events

--- public/app/app.js
+++ public/app/app.js
@@ -1,39 +1,15 @@
 var app = angular.module('myApp',['ngResource']);
 
 app.factory('Event',function($resource){
-	return $resource('/api/events/:id',{ id:'@_id' },{
-		update:{ method:'PUT'}
-	});
+	return $resource('/api/eventsOOM')
 });
 
-app.controller('eventController',function($scope,Event){
+app.controller('eventController',function($scope,$http){
+	$http({
+	method:'GET',
+	url: 'api/eventsOOM'
+	}).then (function(response){$scope.eventsOOM = response.data
+	},function (error){
+	});
 	
-	$scope.event = new Event();
-	var refresh = function(){
-		$scope.events = Event.query();
-		$scope.event = "";
-	}
-	refresh();
-	
-	$scope.remove = function(event){
-		event.$delete(function(){
-			refresh();
-		});
-	}
-	
-	$scope.add = function(event){
-		Event.save(event,function(event){
-			refresh();
-		});
-	}
-	
-	$scope.edit = function(id){
-		$scope.event = Event.get({id:id});
-	}
-	
-	$scope.update = function(event){
-		event.$update(function(){
-			refresh();
-		});
-	}
 });
